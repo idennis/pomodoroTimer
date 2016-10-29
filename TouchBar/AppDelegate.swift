c75a850db6cb1c08d2ea7be5bfa252701b0a7a25
Chris Ricker <chrisricker@chriss-macbook-pro.local>
2016-10-28 23:01:56 -0400

    Initial Commit

---
+++ TouchBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBar
+//
+//  Created by Chris Ricker on 10/28/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBar
+//
+//  Created by Chris Ricker on 10/28/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
